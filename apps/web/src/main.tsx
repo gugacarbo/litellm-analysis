6224cb1e96bcfcee1952325f01fc570bd2c3d48d
Gustavo Carbonera <gustavo.carbonera@atplus.com.br>
2026-04-29 14:46:14 -0300

    a

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,13 +1,13 @@
-import { QueryClientProvider } from "@tanstack/react-query";
 import { TanStackDevtools } from "@tanstack/react-devtools";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
 import { createRoot } from "react-dom/client";
 
 import "./styles/globals.css";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
-import App from "./App.tsx";
 import { Toaster } from "@/components/ui/sonner.tsx";
 import { queryClient } from "@/lib/query-client";
+import App from "./App.tsx";
 
 const rootEl = document.getElementById("root");
 if (!rootEl) throw new Error("Root element not found");
